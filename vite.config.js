0ea8b9c4a4511cea46562bab6d6968085787a65b
duedev <70658447+duedev@users.noreply.github.com>
2025-07-14 22:28:56 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/career-advisor-spa/' // Replace with your repo name for GitHub Pages
+  base: '/career-advisor-spa/'  // Your repo name here
 })
